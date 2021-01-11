f5323da41219446c893c2b2fb8f2f32aa4264d5a
Óscar Vega <s16120245@alumnos.itsur.edu.mx>
2021-01-10 18:47:21 -0600

    Filtro de etiquetas

--- resources/views/posts/tag.blade.php
+++ resources/views/posts/tag.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="max-w-5xl mx-auto px-2 sm:px-6 lg:px-8 py-8">
-        <h1 class="uppercase text-center text-3xl font-bold mb-4">Categoría: {{ $category->name }}</h1>
+        <h1 class="uppercase text-center text-3xl font-bold mb-4">Etiqueta: {{ $tag->name }}</h1>
         @foreach ($posts as $post)
             <x-card-post :post="$post" />
         @endforeach
