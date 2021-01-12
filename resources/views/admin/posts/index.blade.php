59c34efebfbbdeb3e9bb123f8943246a7c299340
Óscar Vega <s16120245@alumnos.itsur.edu.mx>
2021-01-11 23:37:47 -0600

    Listado de posts

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,18 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <a class="btn btn-success float-right" href="{{ route('admin.tags.create') }}">Agregar etiqueta</a>
+    <h1>Lista de posts</h1>
+@stop
+
+@section('content')
+    @if (session('info'))
+        <div class="alert alert-success">
+            <strong>{{ session('info') }}</strong>
+        </div>
+    @endif
+    <p>Aquí se listan posts.</p>
+    @livewire('admin.posts-index')
+@stop
