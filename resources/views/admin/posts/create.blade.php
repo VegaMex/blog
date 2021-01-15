bf704e0f55db7239e713ac43189d1c7e6e8f8520
Óscar Vega <s16120245@alumnos.itsur.edu.mx>
2021-01-15 14:19:29 -0600

    Crear posts

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -11,17 +11,27 @@
     <div class="card">
         <div class="card-body">
             {!! Form::open([ 'route' => 'admin.posts.store', 'autocomplete' => 'off']) !!}
+                {!! Form::hidden('user_id', auth()->user()->id) !!}
                 <div class="form-group">
                     {!! Form::label('name', 'Nombre') !!}
                     {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el nombre del post']) !!}
+                    @error('name')
+                        <small class="text-danger">{{ $message }}</small>
+                    @enderror
                 </div>
                 <div class="form-group">
                     {!! Form::label('slug', 'Slug') !!}
                     {!! Form::text('slug', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el nombre del post', 'readonly']) !!}
+                    @error('slug')
+                        <small class="text-danger">{{ $message }}</small>
+                    @enderror
                 </div>
                 <div class="form-group">
                     {!! Form::label('category', 'Categoría') !!}
                     {!! Form::select('category_id', $categories, null, ['class' => 'form-control']) !!}
+                    @error('category_id')
+                        <small class="text-danger">{{ $message }}</small>
+                    @enderror                    
                 </div>
                 <div class="form-group">
                     <p class="font-weight-bold">
@@ -33,6 +43,9 @@
                             {{ $tag->name }}
                         </label>
                     @endforeach
+                    @error('tags')
+                        <small class="text-danger">{{ $message }}</small>
+                    @enderror                    
                 </div>
                 <div class="form-group">
                     <p class="font-weight-bold">
@@ -50,10 +63,16 @@
                 <div class="form-group">
                     {!! Form::label('extract', 'Extracto') !!}
                     {!! Form::textarea('extract', null, ['class' => 'form-control']) !!}
+                    @error('extract')
+                        <small class="text-danger">{{ $message }}</small>
+                    @enderror                    
                 </div>
                 <div class="form-group">
                     {!! Form::label('body', 'Cuerpo del post') !!}
                     {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+                    @error('body')
+                        <small class="text-danger">{{ $message }}</small>
+                    @enderror                    
                 </div>
                 {!! Form::submit('Crear post', ['class' => 'btn btn-primary']) !!}
             {!! Form::close() !!}
